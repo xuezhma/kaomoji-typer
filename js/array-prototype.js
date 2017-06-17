b1380f2cdac6b7ddd8755a45118d620dbbd975be
Xuezheng Ma <xuezhma@gmail.com>
2017-06-16 18:01:27 -0700

    Use better forEach and map

---
+++ js/array-prototype.js
@@ -0,0 +1,11 @@
+"use strict"
+
+Array.prototype.forEach = function(a) {
+	for(let i = 0; i < this.length; i++) a(this[i], i, this)
+ }
+
+ Array.prototype.map = function(a) {
+	 const array = new Array(this.length)
+	 for(let i = 0; i < this.length; i++) array[i] = a(this[i], i, this)
+	 return array
+ }
